05bf18c9075fd50bcf3ff0be4f91d1a82095fc7d
Nicolo <nicolorizzi1996@gmail.com>
2020-05-04 13:17:26 +0200

    FIXING AJAX

--- js/main.js
+++ js/main.js
@@ -12,7 +12,25 @@ $(document).ready(function () {
   var movieApi = 'https://api.themoviedb.org/3/search/movie';
 
   $(submitBtn).click(function () {
+    //ottengo il valore dalla input
     var searchMovie = input.val().trim().toLowerCase();
+    // chiamata Ajax
+    $.ajax({
+      url: movieApi,
+      type: 'GET',
+      data: {
+        api_key:'e98fb56b8c3a7d7884b98e6cff128fde',
+        query: searchMovie,
+        language: 'it-IT'
+      },
+      success: function (res) {
+        console.log(res.results);
+        
+      },
+      error: function(){
+        console.error('Errore chiamata Ajax');
+      }
+    });
     
   }); // ----- END CLICK SUBMIT
 }); // ----- END DOC READY
