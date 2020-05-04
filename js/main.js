a5dfd53435d91fb0f6601f1378ecae371819d449
Nicolo <nicolorizzi1996@gmail.com>
2020-05-04 15:19:13 +0200

    GET ALL LOGIC IN A FUNCTION

--- js/main.js
+++ js/main.js
@@ -12,37 +12,45 @@ $(document).ready(function () {
   var movieApi = 'https://api.themoviedb.org/3/search/movie';
 
   $(submitBtn).click(function () {
-    //ottengo il valore dalla input
-    movieList.children().remove();
-    var searchMovie = input.val().trim().toLowerCase();
-    input.val('');
-    // chiamata Ajax
-    $.ajax({
-      url: movieApi,
-      type: 'GET',
-      data: {
-        api_key:'e98fb56b8c3a7d7884b98e6cff128fde',
-        query: searchMovie,
-        language: 'it-IT'
-      },
-      success: function (res) {
-        var movies = res.results;
-        for(var i = 0; i < movies.length; i++) {
-          var thisMovie = movies[i];
-          var context = {
-            original_title: thisMovie.original_title,
-            title: thisMovie.title,
-            original_language: thisMovie.original_language,
-            vote_average: thisMovie.vote_average
-          }
-          var html = template(context);
-          $('.movie-list').append(html);
-        }
-      },
-      error: function(){
-        console.error('Errore chiamata Ajax');
-      }
-    });
-    
+    getMovies(template,input,movieList,movieApi)
   }); // ----- END CLICK SUBMIT
 }); // ----- END DOC READY
+
+/**
+ * FUNCTION
+ */
+function getMovies (template,input,movieList,movieApi) {
+  //ottengo il valore dalla input
+  var searchMovie = input.val().trim().toLowerCase();
+  //pulizia del campo
+  input.val('');
+  // rimuovo contenuto precedente
+  movieList.children().remove();
+  // chiamata Ajax
+  $.ajax({
+    url: movieApi,
+    type: 'GET',
+    data: {
+      api_key: 'e98fb56b8c3a7d7884b98e6cff128fde',
+      query: searchMovie,
+      language: 'it-IT'
+    },
+    success: function (res) {
+      var movies = res.results;
+      for (var i = 0; i < movies.length; i++) {
+        var thisMovie = movies[i];
+        var context = {
+          original_title: thisMovie.original_title,
+          title: thisMovie.title,
+          original_language: thisMovie.original_language,
+          vote_average: thisMovie.vote_average
+        }
+        var html = template(context);
+        $('.movie-list').append(html);
+      }
+    },
+    error: function () {
+      console.error('Errore chiamata Ajax');
+    }
+  });
+}
