faba544d38a7e48fdffedf0959bf83b9d125c688
Nicolo <nicolorizzi1996@gmail.com>
2020-05-04 13:09:18 +0200

    GET VALUE FROM INPUT DATA

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ $(document).ready(function () {
   /**
    * INIT HANDLEBARS
    */
-
   var source = $('#movie-template').html();
   var template = Handlebars.compile(source);
-  
 
+  // references
+  var submitBtn = $('#submit');
+  var input = $('#search');
+  var movieList = $('.movie-list');
+  var movieApi = 'https://api.themoviedb.org/3/search/movie';
 
-
-  var html = template(context)
-  $('#movie-list').append(html);
+  $(submitBtn).click(function () {
+    var searchMovie = input.val().trim().toLowerCase();
+    
+  }); // ----- END CLICK SUBMIT
 }); // ----- END DOC READY
