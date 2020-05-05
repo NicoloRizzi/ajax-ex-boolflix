a66db9d09f5c5742270d8f853c8bf9d7ccd5cbbc
Nicolo <nicolorizzi1996@gmail.com>
2020-05-05 12:57:44 +0200

    ADDED FUNCTION PRINTSTAR

--- js/main.js
+++ js/main.js
@@ -7,17 +7,16 @@ $(document).ready(function () {
 
   // references
   var submitBtn = $('#submit');
-  var input = $('#search');
+  var querySearch = $('#search');
   var movieList = $('.movie-list');
-  var movieApi = 'https://api.themoviedb.org/3/search/movie';
 
-  $(submitBtn).click(function () {
-    getMovies(template,input,movieList,movieApi)
+  submitBtn.click(function () {
+    getMovies(template,querySearch,movieList);
   }); // ----- END CLICK SUBMIT
   
-  input.keyup(function (e) {
+  querySearch.keyup(function (e) {
     if (e.which == 13) {
-      getMovies(template, input, movieList, movieApi)
+      getMovies(template,querySearch,movieList);
     }
   });
 }); // ----- END DOC READY
@@ -25,38 +24,56 @@ $(document).ready(function () {
 /**
  * FUNCTION
  */
-function getMovies (template,input,movieList,movieApi) {
-  //ottengo il valore dalla input
-  var searchMovie = input.val().trim().toLowerCase();
-  //pulizia del campo
-  input.val('');
-  // rimuovo contenuto precedente
-  movieList.children().remove();
-  // chiamata Ajax
-  $.ajax({
-    url: movieApi,
-    type: 'GET',
-    data: {
-      api_key: 'e98fb56b8c3a7d7884b98e6cff128fde',
-      query: searchMovie,
-      language: 'it-IT'
-    },
-    success: function (res) {
-      var movies = res.results;
-      for (var i = 0; i < movies.length; i++) {
-        var thisMovie = movies[i];      
-        var context = {
-          original_title: thisMovie.original_title,
-          title: thisMovie.title,
-          original_language: thisMovie.original_language,
-          vote_average: thisMovie.vote_average
+function getMovies(template,querySearch,movieList) {
+  var searchMovie = querySearch.val().trim().toLowerCase();
+  if(searchMovie !=="") {
+    // chiamata Ajax
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie",
+      type: 'GET',
+      data: {
+        api_key: 'e98fb56b8c3a7d7884b98e6cff128fde',
+        query: searchMovie,
+        language: 'it-IT'
+      },
+      success: function (data) {
+        var movies = data.results;
+        // rimuovo contenuto precedente
+        movieList.children().remove(); 
+        for (var i = 0; i < movies.length; i++) {
+          var thisMovie = movies[i];          
+          var context = {
+            original_title: thisMovie.original_title,
+            title: thisMovie.title,
+            original_language: thisMovie.original_language,
+            vote_average: printStar(thisMovie),
+            type: 'Film'
+          }
+          var html = template(context);
+          movieList.append(html);
         }
-        var html = template(context);
-        movieList.append(html);
+      },
+      error: function () {
+        console.error('Errore chiamata Ajax');
       }
-    },
-    error: function () {
-      console.error('Errore chiamata Ajax');
-    }
-  });
+    });
+  } else {
+    alert('Prego, inserire un valore nella ricerca');
+    //pulizia del campo
+    querySearch.val('');
+  }
 } // END FUNCTION
+
+function printStar (element){
+
+  var rating = Math.round(element.vote_average / 2);
+  var stars = "";
+  for (var i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars += '<i class="fas fa-star"></i>';
+    } else {
+      stars += '<i class="far fa-star"></i>';
+    }
+  }
+  return stars;
+}
